caa94ea508d6cfdfedc19c4ccf41b17dee5457c6
kamikawa <kamikawa@inpact.ne.jp>
2021-11-05 10:11:41 +0900

    ログ出力機能の作成
    ログ出力機能についての学習のため

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,80 +1,54 @@
 ﻿using System;
+using CalculatorLibrary;
 
-namespace Calculator
+namespace CalculatorProgram
 {
-    class Calculator
-    {
-        public static double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-            // Use a switch statement to do the math.
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor.
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    break;
-                // Return text for an incorrect option entry.
-                default:
-                    break;
-            }
-            return result;
-        }
-    }
-
-
     class Program
     {
         static void Main(string[] args)
         {
             bool endApp = false;
-            // Display title as the C# console calculator app.
+            // 最初の表示内容
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
 
+            // CalculatorLibraryのCalclator()を呼び出し
+            Calculator calculator = new Calculator();
+
             while (!endApp)
             {
-                // Declare variables and set to empty.
+                // 初期値の設定
                 string numInput1 = "";
                 string numInput2 = "";
                 double result = 0;
 
-                // Ask the user to type the first number.
+                // 1つ目の数字
                 Console.Write("Type a number, and then press Enter: ");
                 numInput1 = Console.ReadLine();
 
                 double cleanNum1 = 0;
+
+                // 入力した値がdouble型に変換できない場合、もう一度入力を求める
                 while (!double.TryParse(numInput1, out cleanNum1))
                 {
                     Console.Write("This is not valid input. Please enter an integer value: ");
                     numInput1 = Console.ReadLine();
                 }
 
-                // Ask the user to type the second number.
+                // 2つ目の数字
                 Console.Write("Type another number, and then press Enter: ");
                 numInput2 = Console.ReadLine();
 
                 double cleanNum2 = 0;
+
+                // 入力した値がdouble型に変換できない場合、もう一度入力を求める
                 while (!double.TryParse(numInput2, out cleanNum2))
                 {
                     Console.Write("This is not valid input. Please enter an integer value: ");
                     numInput2 = Console.ReadLine();
                 }
 
-                // Ask the user to choose an operator.
+                // 四則演算のどれをするか聞く
                 Console.WriteLine("Choose an operator from the following list:");
                 Console.WriteLine("\ta - Add");
                 Console.WriteLine("\ts - Subtract");
@@ -82,16 +56,20 @@ namespace Calculator
                 Console.WriteLine("\td - Divide");
                 Console.Write("Your option? ");
 
+                // 入力された文字の取得
                 string op = Console.ReadLine();
 
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    // CalculatorLibraryのDoOperation実行
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op); 
+
+                    // 正常な計算ができているかの判断
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
                     }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    else Console.WriteLine("Your result: " + result + "\n");
                 }
                 catch (Exception e)
                 {
@@ -100,12 +78,15 @@ namespace Calculator
 
                 Console.WriteLine("------------------------\n");
 
-                // Wait for the user to respond before closing.
+                // 続けて計算するかどうか
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n") endApp = true;
 
-                Console.WriteLine("\n"); // Friendly linespacing.
+                Console.WriteLine("\n");
             }
+
+            // CalclatorLibraryのFinish()の実行
+            calculator.Finish();
             return;
         }
     }
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+        public Calculator()
+        {
+            // ログ出力するファイルの作成 binフォルダ内に保存される
+            StreamWriter logFile = File.CreateText("calculator.log");
+
+            // 書き込み後Flushを呼び出す
+            Trace.AutoFlush = true;
+
+            // 新しくテキストファイル形式でログファイルを作る
+            writer = new JsonTextWriter(logFile);
+
+            // 配列ごとにインデント、改行しログに出力する
+            writer.Formatting = Formatting.Indented;
+
+            // オブジェクトの始まりを書き込み
+            writer.WriteStartObject();
+
+            // 配列名
+            writer.WritePropertyName("Operations");
+
+            // 配列の始まりを書き込み
+            writer.WriteStartArray();
+
+            // ログへの出力内容
+            Trace.WriteLine("Starting Calculator Log");
+            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+        }
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            
+            // JSONオブジェクトの先頭を読み込み
+            writer.WriteStartObject();
+
+            // Operand1をログに書き込み
+            writer.WritePropertyName("Operand1");
+
+            // num1をログに書き込む
+            writer.WriteValue(num1);
+
+            // Operand2をログに書き込み
+            writer.WritePropertyName("Operand2");
+
+            // num2をログに書き込む
+            writer.WriteValue(num2);
+
+            // Operationをログに書き込み
+            writer.WritePropertyName("Operation");
+
+            // 入力された文字によって四則演算を行う
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    // Addをログに書き込む
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    // Subtractをログに書き込む
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    // Multiplyをログに書き込む
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    // 0除算ではないかどうかの判断
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        // Divideをログに書き込む
+                        writer.WriteValue("Divide");
+                    }
+                    break;
+                // 上記以外の文字などが入力されたとき
+                default:
+                    break;
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+        public void Finish()
+        {
+            // JSON配列の終了を書き込み
+            writer.WriteEndArray();
+
+            // オブジェクトの終わりを書き込み
+            writer.WriteEndObject();
+
+            // フォームを閉じる
+            writer.Close();
+        }
+    }
+}
